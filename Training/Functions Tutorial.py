2de6be80ebc4c76372649520cb9bc20908462f7d
ramazan <ramazansakrs@gmail.com>
2020-12-31 17:51:00 +0100

    feat(Training) Add new Functions - Even numbers

--- Training/Functions Tutorial.py
+++ Training/Functions Tutorial.py
@@ -1,7 +1,12 @@
+
+
 def hello():
     print("Hello world!")
 
 hello()
+
+# hello() We can  not call the function before define it (At first line we can not call the function)
+
 input()
 
 def print_with_exclamation(word):
@@ -21,4 +26,27 @@ def print_sum_twice(x, y):
 print_sum_twice(5, 8)
 
 input()
+
+def function(variable):  # Takes one argument
+    variable += 1
+    print(variable)
+
+function(7)
+
+input()
+
+#Function voor even numbers
+
+def even(x):
+    
+    if x % 2 == 0:
+        print("Number is Even.")
+    else:
+        print("Number is Odd")
+
+even(5)
+even(6)
+input()
+
+
 
