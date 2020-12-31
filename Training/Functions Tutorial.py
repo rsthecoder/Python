78a0f99a7cfa2e246ea87f9a4fcbeffcbe1241c9
ramazan <ramazansakrs@gmail.com>
2020-12-31 17:42:16 +0100

    feat(Training) Add new tutorial Functions

---
+++ Training/Functions Tutorial.py
@@ -0,0 +1,24 @@
+def hello():
+    print("Hello world!")
+
+hello()
+input()
+
+def print_with_exclamation(word):
+   print(word + "!")
+    
+print_with_exclamation("spam")
+print_with_exclamation("eggs")
+print_with_exclamation("python")
+
+input()
+
+
+def print_sum_twice(x, y):
+   print(x + y)
+   print(x + y)
+
+print_sum_twice(5, 8)
+
+input()
+
