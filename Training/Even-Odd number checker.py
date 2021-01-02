feba17a5936923ba6609baefa54767d1fce90820
ramazan <ramazansakrs@gmail.com>
2021-01-02 18:47:43 +0100

    feat(training) Add even - odd number checker - comments

--- Training/Even-Odd number checker.py
+++ Training/Even-Odd number checker.py
@@ -1,12 +1,12 @@
 
 while True:
-    number = int(input("\nEnter a numnber to check even or odd: "))
+    number = int(input("\nEnter a numnber to check even or odd: ")) #User enters a number
     try:
-        if number % 2 == 0:
+        if number % 2 == 0: # then this is an even number
             print("\nNumber is even")
             continue
-        else:
+        else:  # then this is an odd number
             print("\nNumber is an odd number")
             continue
-    except:
+    except: 
         print("\nPlease enter a number!")
